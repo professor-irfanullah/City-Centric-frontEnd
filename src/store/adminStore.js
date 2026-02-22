9bf9ffe57aaefe99001facac2d8ed9e685d6ccff
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2026-02-23 02:10:23 +0500

    added more methods to the admin store

--- src/store/adminStore.js
+++ src/store/adminStore.js
@@ -26,7 +26,33 @@ const adminStore = defineStore("adminReports", () => {
             throw error;
         }
     }
-    return { fetchAllReports, reports, fetchAdminAnalytics, analytics };
+    const fetchSingleReport = async (reportId) => {
+        try {
+            const response = await api.get(`/api/admin/get/report?reportId=${reportId}`)
+            return response.data;
+        } catch (error) {
+            throw error
+        }
+    }
+    const downloadReportInPDF = async (reportId) => {
+        try {
+            const response = await api.get(`/api/admin/download-pdf?report_id=${reportId}`, {
+                responseType: 'blob',
+            })
+            const blob = await response.data
+            const url = window.URL.createObjectURL(blob)
+            const link = document.createElement('a')
+            link.href = url
+            link.setAttribute('download', `Report-${reportId}.pdf`)
+            document.body.appendChild(link)
+            link.click()
+            link.remove()
+            window.URL.revokeObjectURL(url)
+        } catch (error) {
+            throw error
+        }
+    }
+    return { fetchAllReports, reports, fetchAdminAnalytics, analytics, fetchSingleReport, downloadReportInPDF };
 
 })
 export default adminStore;
